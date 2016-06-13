3b5e2a4c27fe702846c5b061a9be3b5a5627b4a7
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-13 20:22:56 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -8,8 +8,8 @@ ERROR_EXIT () {
   exit 1
 }
  
-echo '12' > $tmp_ans
-./comFactor 12 36 > $tmp_out || ERROR_EXIT "TEST1-1"
+echo "12" > $tmp_ans
+./comFactor "12" "36" > $tmp_out || ERROR_EXIT "TEST1-1"
 diff $tmp_ans $tmp_out || ERROR_EXIT "TEST1-2"
 
 echo OK
