3091dc49986fdd8a876a80ba38f717153f3a91b3
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-13 20:09:06 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -4,13 +4,13 @@ tmp=/tmp/$$
 
 ERROR_EXIT () {
   echo "$1" >&2
-  rm -f $tmp*
+  rm -f $tmp_*
   exit 1
 }
  
-echo 12 >$tmpans
-./comFactor 12 36 >$tmpout || ERROR_EXIT "TEST1-1"
-diff $tmpans $tmpout || ERROR_EXIT "TEST1-2"
+echo 12 > $tmp_ans
+./comFactor 12 36 > $tmp_out || ERROR_EXIT "TEST1-1"
+diff $tmp_ans $tmp_out || ERROR_EXIT "TEST1-2"
 
 echo OK
 rm -f $temp*
