6d44cd5d3b90a218c39ce1257f2bbc8b5be5eb15
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-13 19:11:30 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
-#!/bin/bash
+#bin/bash
 
-echo "hello test"
+temp=/temp/$$
+
+ERROR_EXIT () {
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
+}
+ 
+echo 12 > $tmp-ans
+./ComFactor 12 36 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo OK
+rm -f $temp-*
+exit 0
+
