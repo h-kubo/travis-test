7a8939acd58231343db4b9b41d88457099880949
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-17 18:56:59 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -5,14 +5,30 @@ tmp=/tmp/$$
 ERROR_EXIT () {
   echo "$1" >&2
   rm -f $tmp_*
+  echo NG
   exit 1
 }
  
 echo "12" > tmp_ans
 ./comFactor "12" "36" > tmp_out || ERROR_EXIT "TEST1-1"
 diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
-
 echo OK
 rm -f $temp*
+
+
+echo "12" > tmp_ans
+./comFactor "15" "36" > tmp_out || ERROR_EXIT "TEST1-1"
+diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
+echo OK
+rm -f $temp*
+
+
+echo "12" > tmp_ans
+./comFactor "a" "36" > tmp_out || ERROR_EXIT "TEST1-1"
+diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
+echo OK
+rm -f $temp*
+
+
 exit 0
 
