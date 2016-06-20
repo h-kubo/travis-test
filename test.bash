b32fb4cbf21801689240c7000165e3448be20dbb
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-20 22:09:37 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -10,20 +10,20 @@ ERROR_EXIT () {
 }
  
 echo "12" > tmp_ans
-./comFactor "12" "36" > tmp_out || ERROR_EXIT "TEST1-1"
+./comFactor "12" "36" > tmp_out && ERROR_EXIT "TEST1-1"
 diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
 echo OK
 
 
 echo "12" > tmp_ans
-./comFactor "15" "36" > tmp_out || ERROR_EXIT "TEST1-1"
-diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
+./comFactor "15" "36" > tmp_out && ERROR_EXIT "TEST2-1"
+diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST2-2"
 echo OK
 
 
 echo "12" > tmp_ans
-./comFactor "a" "36" > tmp_out || ERROR_EXIT "TEST1-1"
-diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST1-2"
+./comFactor "a" "36" > tmp_out && ERROR_EXIT "TEST3-1"
+diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST3-2"
 echo OK
 
 rm -f $temp*
