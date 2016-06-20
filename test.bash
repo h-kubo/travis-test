d51d37ee582b7b2a675b558dc22b928993483099
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-20 22:30:07 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -16,6 +16,7 @@ echo OK
 rm -f $temp*
 
 
+tmp=/tmp/$$
 echo "12" > tmp_ans
 ./comFactor "15" "36" > tmp_out || ERROR_EXIT "TEST2-1"
 diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST2-2"
@@ -23,6 +24,7 @@ echo OK
 rm -f $temp*
 
 
+tmp=/tmp/$$
 ./comFactor "a" "36" > tmp_out && ERROR_EXIT "TEST3-1"
 echo OK
 
