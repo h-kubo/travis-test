85ba1d64f9faec7d618aeab9ed2a1f9ac57aafa6
kubo hiroshi <z13026hk@aiit.ac.jp>
2016-06-20 22:24:32 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -21,10 +21,8 @@ diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST2-2"
 echo OK
 
 
-echo "12" > tmp_ans
 ./comFactor "a" "36" > tmp_out && ERROR_EXIT "TEST3-1"
-diff <($tmp_ans) <($tmp_out) || ERROR_EXIT "TEST3-2"
-echo NG
+echo OK
 
 rm -f $temp*
 exit 0
